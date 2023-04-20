51932ec18f42f96b47455838c1fc767bf39d9f4b
tatfra <fraga.ribeiro.tatiana@gmail.com>
2023-04-20 23:07:15 +0100

    display forecast data using the api

--- src/app.js
+++ src/app.js
@@ -45,28 +45,45 @@ fullDate.innerHTML = `${year} ${month}, ${date}`;
 let dayTime = document.querySelector("#day");
 dayTime.innerHTML = `${day}, ${hour}:${minutes}`;
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
   let forecastHTML = `<div class="row">`;
 
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
     <div class="col-2">
-      <div class="weather-forecast-date">${day}</div>
-      <img src="https://openweathermap.org/img/wn/01n@2x.png" alt="" width="42" />
+      <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+    
+      <img src="https://openweathermap.org/img/wn/${
+        forecastDay.weather[0].icon
+      }@2x.png" alt="" width="42" />
       <div class="weather-forecast-temperature">
-        <span class="weather-forecast-temperature-max">18º</span>
-        <span class="weather-forecast-temperature-min">12º</span>
+        <span class="weather-forecast-temperature-max">${Math.round(
+          forecastDay.temp.max
+        )}º</span>
+        <span class="weather-forecast-temperature-min">${Math.round(
+          forecastDay.temp.min
+        )}º</span>
     </div>
 `;
 
-    forecastHTML = forecastHTML + `</div>`;
-    forecastElement.innerHTML = forecastHTML;
+      forecastHTML = forecastHTML + `</div>`;
+      forecastElement.innerHTML = forecastHTML;
+    }
   });
 }
 
